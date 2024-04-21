7f539c193e7c2647d0d02d6092c786e1d71c1d51
Pedro Balbino <phsb5321@gmail.com>
2024-04-21 14:11:24 -0300

    Enable OpenSSH daemon and disable root login

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -143,7 +143,10 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    permitRootLogin = "no"; # or "yes" if you want to enable root login
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
