40fdfdcafbb47a24d0abbbae8c002481c9a724c6
Pedro Balbino <phsb5321@gmail.com>
2024-05-01 13:20:42 -0300

    64 current  2024-05-01 13:20:37  24.05.20240427.58a1abd  6.6.28                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -93,6 +93,8 @@
 
   # Gaming and applications
   programs.fish.enable = true;
+  # Set the default shell to fish
+  users.users.notroot.shell = pkgs.fish;
   programs.steam.enable = true;
   programs.steam.remotePlay.openFirewall = true;
   programs.steam.dedicatedServer.openFirewall = true;
