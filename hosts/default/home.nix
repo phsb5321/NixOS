e13e0cee2d3053934431d011fdb34dfbf8251c91
Pedro Balbino <phsb5321@gmail.com>
2024-05-01 13:51:54 -0300

    70 current 2024-05-01 13:51:49 24.05.20240427.58a1abd 6.6.28 *

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -16,8 +16,32 @@
     fish
   ];
 
-  # Set fish as the default shell
-  programs.fish.enable = true;
+  programs.fish = {
+    enable = true;
+    interactiveShellInit = ''
+      set fish_greeting # Disable greeting
+    '';
+    plugins = [
+      # Enable a plugin (here grc for colorized command output) from nixpkgs
+      { name = "grc"; src = pkgs.fishPlugins.grc.src; }
+      # Manually packaging and enable a plugin
+      {
+        name = "z";
+        src = pkgs.fetchFromGitHub {
+          owner = "jethrokuan";
+          repo = "z";
+          rev = "e0e1b9dfdba362f8ab1ae8c1afc7ccf62b89f7eb";
+          sha256 = "0dbnir6jbwjpjalz14snzd3cgdysgcs3raznsijd6savad3qhijc";
+        };
+      }
+    ];
+  };
+
+  # Environment Variables
+  home.sessionVariables = {
+    EDITOR = "nvim";
+    SHELL = "${pkgs.fish}/bin/fish"; # Environment shell set to Fish
+  };
 
   # Dotfiles Management
   home.file = {
@@ -28,13 +52,7 @@
     # '';
   };
 
-  # Environment Variables
-  home.sessionVariables = {
-    EDITOR = "nvim";
-    # Shell Configuration => Fish Shell
-    SHELL = "${pkgs.fish}/bin/fish";
 
-  };
 
   # Home Manager Self-Management
   programs.home-manager.enable = true;
