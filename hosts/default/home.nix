26eef54120417c844bf7b89fe7a9c293280d7aed
Pedro Balbino <phsb5321@gmail.com>
2024-05-01 12:57:48 -0300

    58 current 2024-05-01 12:45:02 24.05.20240427.58a1abd 6.6.28 *

--- hosts/default/home.nix
+++ hosts/default/home.nix
@@ -4,115 +4,65 @@
   imports = [
     inputs.nixvim.homeManagerModules.nixvim
   ];
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
+
+  # User and Home Directory Configuration
   home.username = "notroot";
   home.homeDirectory = "/home/notroot";
+  home.stateVersion = "23.11"; # Compatible Home Manager release version
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "23.11"; # Please read the comment before changing.
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    (pkgs.nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+  # Package Installation
+  home.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
+  # Dotfiles Management
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
     # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
     # ".gradle/gradle.properties".text = ''
     #   org.gradle.console=verbose
     #   org.gradle.daemon.idletimeout=3600000
     # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. If you don't want to manage your shell through Home
-  # Manager then you have to manually source 'hm-session-vars.sh' located at
-  # either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/notroot/etc/profile.d/hm-session-vars.sh
-  #
+  # Environment Variables
   home.sessionVariables = {
     # EDITOR = "emacs";
   };
 
-  # Let Home Manager install and manage itself.
+  # Home Manager Self-Management
   programs.home-manager.enable = true;
 
-  # NixVim
+  # NixVim Configuration
   programs.nixvim = {
     enable = true;
-
     enableMan = true;
+    opts = {
+      number = true; # Show line numbers
+      relativenumber = true; # Show relative line numbers
 
-    # Plugins
+      shiftwidth = 2; # Tab width should be 2
+    };
     plugins = {
       bufferline.enable = true;
       lualine.enable = true;
       fzf-lua.enable = true;
-      dashboard.enable = true;
+      dashboard = {
+        enable = true;
+      };
       nvim-tree.enable = true;
       treesitter.enable = true;
-
+      obsidian.enable = true;
       lsp = {
         enable = true;
-
         servers = {
-          # javascript / typescript
-          tsserver.enable = true;
-
-          # lua
-          lua-ls.enable = true;
-
-          # rust
-          rust-analyzer.enable = true;
+          tsserver.enable = true; # JavaScript/TypeScript
+          lua-ls.enable = true; # Lua
+          rust-analyzer = { enable = true; installCargo = true; installRustc = true; }; # Rust
         };
       };
     };
-
-    # Keymaps
     keymaps = [
-      {
-        key = "<leader>n";
-        action = "<cmd>NvimTreeToggle<cr>";
-      }
+      { key = "<leader>n"; action = "<cmd>NvimTreeToggle<cr>"; }
     ];
   };
 }
