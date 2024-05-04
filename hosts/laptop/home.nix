b8f2ec947b3e6d83b30d568506391e82b30480ff
Pedro Balbino <phsb5321@gmail.com>
2024-05-04 15:51:25 -0300

    Update laptop configuration and add Jupyter shell

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -44,12 +44,13 @@
     enable = true;
     theme = "Tokyo Night";
     font.name = "JetBrainsMono Nerd Font";
+    shellIntegration.enableFishIntegration = true;
     settings = {
-      confirm_os_window_close = -0;
       copy_on_select = true;
       clipboard_control = "write-clipboard read-clipboard write-primary read-primary";
     };
   };
+
 
   # Environment Variables
   home.sessionVariables = {
