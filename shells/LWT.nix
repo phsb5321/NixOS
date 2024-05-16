c800d20200ff4ac734f6e1dd0271761e917480e5
Pedro Balbino <phsb5321@gmail.com>
2024-05-16 16:07:15 -0300

    118 current 2024-05-16 16:06:58 24.05.20240515.33d1e75 6.6.30 *

--- shells/LWT.nix
+++ shells/LWT.nix
@@ -1,13 +1,16 @@
 { pkgs ? import <nixpkgs> { } }:
 
 pkgs.mkShell {
-
   buildInputs = [
     pkgs.bun
     pkgs.sqlite
+    pkgs.corepack_22
   ];
 
   shellHook = ''
+    export PNPM_HOME="$HOME/.local/share/pnpm"
+    export PATH=$PNPM_HOME/bin:$PATH
+    pnpm setup
     export PATH=$PATH:${pkgs.bun}/bin
   '';
 }
