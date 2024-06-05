0feaa2ac6857ed7ae73a2bb7cdcec476fd063a3f
Pedro Balbino <phsb5321@gmail.com>
2024-06-05 18:56:03 -0300

    154 current  2024-06-05 18:55:53  24.11.20240529.ad57eef  6.6.32                          *

--- shells/CITi.nix
+++ shells/CITi.nix
@@ -2,12 +2,15 @@
 
 pkgs.mkShell {
   buildInputs = [
+    pkgs.bun
+    pkgs.corepack_22
     pkgs.nodejs_22
-    pkgs.openssl
   ];
 
-
   shellHook = ''
-    export PATH=$PATH:$(npm bin)
+    export PNPM_HOME="$HOME/.local/share/pnpm"
+    export PATH=$PNPM_HOME/bin:$PATH
+    pnpm setup
+    export PATH=$PATH:${pkgs.bun}/bin
   '';
 }
