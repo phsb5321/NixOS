1d4712a801626ea0b5560eb33b8d30f9d4c261a7
Pedro Balbino <phsb5321@gmail.com>
2024-06-29 15:43:21 -0300

    172 current 2024-06-29 15:42:23 24.11.20240529.ad57eef 6.6.32 *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -141,17 +141,12 @@
 
   # Ollama
   services.ollama = {
-    #package = pkgs.unstable.ollama; # Uncomment if you want to use the unstable channel, see https://fictionbecomesfact.com/nixos-unstable-channel
     enable = true;
-    acceleration = "rocm"; # Or "rocm"
-    #environmentVariables = { # I haven't been able to get this to work myself yet, but I'm sharing it for the sake of completeness
-    # HOME = "/home/ollama";
-    # OLLAMA_MODELS = "/home/ollama/models";
-    # OLLAMA_HOST = "0.0.0.0:11434"; # Make Ollama accesible outside of localhost
-    # OLLAMA_ORIGINS = "http://localhost:8080,http://192.168.0.10:*"; # Allow access, otherwise Ollama returns 403 forbidden due to CORS
-    #};
+    acceleration = "rocm";
+    environmentVariables = {
+      HSA_OVERRIDE_GFX_VERSION = "10.1.0";
+    };
   };
-
 
   # Enable hardware and system services
   hardware.bluetooth.enable = true;
