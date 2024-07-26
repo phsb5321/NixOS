549fbc7b421777197294448d7dcdf5ad837936d3
Pedro Balbino <phsb5321@gmail.com>
2024-07-26 11:42:52 -0300

    188 current  2024-07-26 11:41:09  24.11.20240725.5ad6a14  6.6.41                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -138,7 +138,6 @@
   hardware.bluetooth.enable = true;
   hardware.bluetooth.powerOnBoot = true;
   services.printing.enable = true;
-  sound.enable = true;
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire.enable = true;
