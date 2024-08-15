c73f5bcaf5048d515af0c521d190842783b2f407
Pedro Balbino <phsb5321@gmail.com>
2024-08-15 19:41:40 -0300

    260 current 2024-08-15 18:58:38 24.11.20240814.c3aa7b8 6.10.4 *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -325,4 +325,7 @@
       options = [ "NOPASSWD" ];
     }];
   }];
+
+  nix.settings.trusted-substituters = [ "https://ai.cachix.org" ];
+  nix.settings.trusted-public-keys = [ "ai.cachix.org-1:N9dzRK+alWwoKXQlnn0H6aUx0lU/mspIoz8hMvGvbbc=" ];
 }
