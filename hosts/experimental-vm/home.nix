6b81d35c7fa550c3e09b142c222fbb39afb68f2e
Pedro Balbino <phsb5321@gmail.com>
2024-10-15 11:50:25 -0300

    427 current 2024-10-15 10:38:29 24.11.20241009.5633bcf 6.11.2 *

--- hosts/experimental-vm/home.nix
+++ hosts/experimental-vm/home.nix
@@ -68,32 +68,97 @@
     };
   };
 
+  # NixVim Configuration
   programs.nixvim = {
     enable = true;
     colorschemes.catppuccin.enable = true;
+
+    # Global options
     globals.mapleader = " ";
-    options = {
-      number = true;
-      relativenumber = true;
-      shiftwidth = 2;
-      expandtab = true;
+
+    # General options
+    opts = {
+      number = true; # Show line numbers
+      shiftwidth = 2; # Tab width should be 2
+      # Use xclip to copy to clipboard
       clipboard = "unnamedplus";
     };
+
     plugins = {
       lualine.enable = true;
-      treesitter.enable = true;
+      obsidian = {
+        enable = true;
+        settings = {
+          workspaces = [
+            {
+              name = "Notes";
+              path = "~/Documents/Obsidian/Notes";
+            }
+          ];
+          conceallevel = 2; # Set conceal level (0-2)
+          daily_notes = {
+            folder = "3. Resources/Daily";
+            date_format = "%Y/%B/%d (%A)";
+          };
+        };
+      };
+      treesitter = {
+        enable = true;
+        settings.ensure_installed = "all";
+      };
+      lsp = {
+        enable = true;
+        servers = {
+          rust_analyzer = {
+            enable = true;
+            installCargo = true;
+            installRustc = true;
+          };
+          pyright.enable = true;
+          ts_ls.enable = true;
+          nil_ls.enable = true;
+        };
+      };
       telescope = {
         enable = true;
         keymaps = {
           "<leader>ff" = "find_files";
           "<leader>fg" = "live_grep";
+          "<leader>fb" = "buffers";
+          "<leader>fh" = "help_tags";
         };
       };
       gitsigns.enable = true;
+      neo-tree = {
+        enable = true;
+        closeIfLastWindow = true;
+        window.width = 30;
+      };
       which-key.enable = true;
       comment.enable = true;
+      nvim-autopairs.enable = true;
+      neoscroll.enable = true;
+      indent-blankline.enable = true;
+      markdown-preview.enable = true;
+      dashboard.enable = true;
+      web-devicons.enable = true;
+      copilot-vim = {
+        enable = true;
+        settings = { };
+      };
     };
+    # Additional settings for Obsidian markdown files
+    extraConfigLua = ''
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "markdown",
+        callback = function()
+          vim.opt_local.conceallevel = 2
+          vim.opt_local.concealcursor = 'n'
+        end
+      })
+    '';
   };
 
+  # Home Manager Self-Management
   programs.home-manager.enable = true;
 }
