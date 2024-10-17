1eb9cb5549aeb2d302b92afa2911ff6c46c05800
Pedro Balbino <phsb5321@gmail.com>
2024-10-17 15:28:17 -0300

    WORK IN PROGRESS

---
+++ hosts/modules/desktop/common/default.nix
@@ -0,0 +1,47 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  cfg = config.modules.desktop;
+in {
+  config = mkIf cfg.enable {
+    services.xserver.xkb = {
+      layout = "br";
+      variant = "";
+    };
+
+    environment.systemPackages = cfg.extraPackages;
+
+    fonts.enableDefaultPackages = true;
+
+    services.printing.enable = true;
+
+    security.rtkit.enable = true;
+    services.pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
+
+    hardware.bluetooth.enable = true;
+    services.blueman.enable = true;
+
+    networking.networkmanager.enable = true;
+
+    xdg.portal = {
+      enable = true;
+      extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    };
+
+    services.dbus.enable = true;
+    security.polkit.enable = true;
+
+    hardware.graphics = {
+      enable = true;
+      enable32Bit = true;
+    };
+  };
+}
