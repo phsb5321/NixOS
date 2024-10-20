43b9434e6f7c2d594b24402280de18cc2135a13c
Pedro Balbino <phsb5321@gmail.com>
2024-10-20 13:16:53 -0300

    439 current  2024-10-20 13:13:58  24.11.20241018.4c2fcb0  6.11.4                          *

--- hosts/modules/virtualization/default.nix
+++ hosts/modules/virtualization/default.nix
@@ -23,74 +23,31 @@ in
 
     username = mkOption {
       type = types.str;
-      example = "alice";
+      default = "notroot";
       description = "Username to add to virtualization groups";
     };
   };
 
   config = mkIf cfg.enable {
-    # Libvirtd configuration
+    # Enable libvirtd
     virtualisation.libvirtd = mkIf cfg.enableLibvirtd {
       enable = true;
       qemu = {
+        package = pkgs.qemu_kvm;
         ovmf.enable = true;
         runAsRoot = true;
-        swtpm.enable = true;
-        verbatimConfig = ''
-          unix_sock_group = "libvirtd"
-          unix_sock_rw_perms = "0770"
-          spice_listen = "unix"
-          spice_auto_unix_socket = "on"
-          nographics_allow_host_audio = 1
-          vnc_allow_host_audio = 1
-          spice_gl = "on"
-          spice_rendernode = "/dev/dri/renderD128"
-        '';
       };
-      onBoot = "start";
-      onShutdown = "shutdown";
     };
 
-    # QEMU configuration for SPICE and GL support
-    virtualisation.spiceUSBRedirection.enable = true;
+    # Enable SPICE USB redirection
+    services.spice-vdagentd.enable = true;
+
+    # Set default libvirt URI
     environment.sessionVariables.LIBVIRT_DEFAULT_URI = "qemu:///system";
 
     # Virt-manager configuration
     programs.virt-manager = mkIf cfg.enableVirtManager {
       enable = true;
-    };
-
-    # Default network configuration for libvirtd
-    environment.etc."libvirt/qemu/networks/default.xml".text = ''
-      <network>
-        <name>default</name>
-        <bridge name="virbr0"/>
-        <forward mode="nat"/>
-        <ip address="192.168.122.1" netmask="255.255.255.0">
-          <dhcp>
-            <range start="192.168.122.2" end="192.168.122.254"/>
-          </dhcp>
-        </ip>
-      </network>
-    '';
-
-    # Systemd service to ensure the default network is started
-    systemd.services.libvirtd-default-network = mkIf cfg.enableLibvirtd {
-      description = "libvirtd default network";
-      after = [ "libvirtd.service" ];
-      requires = [ "libvirtd.service" ];
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig = {
-        Type = "oneshot";
-        RemainAfterExit = true;
-        ExecStart = ''
-          ${pkgs.bash}/bin/bash -c '
-            ${pkgs.libvirt}/bin/virsh net-define /etc/libvirt/qemu/networks/default.xml
-            ${pkgs.libvirt}/bin/virsh net-autostart default
-            ${pkgs.libvirt}/bin/virsh net-start default
-          '
-        '';
-      };
     };
 
     # Include necessary packages
@@ -106,12 +63,6 @@ in
     # Enable dconf for virt-manager settings persistence
     programs.dconf.enable = cfg.enableVirtManager;
 
-    # Add the user to required groups for virtualization
-    users.users.${cfg.username}.extraGroups = [
-      "libvirtd"
-      "kvm"
-    ];
-
     # Load necessary kernel modules for virtualization
     boot.kernelModules = [ "kvm-intel" "kvm-amd" ];
 
@@ -122,25 +73,43 @@ in
     '';
 
     # Enable 3D acceleration
-    hardware.opengl.enable = true;
-    hardware.opengl.driSupport32Bit = true;
-
-    # Ensure libvirtd service is running and enabled
-    systemd.services.libvirtd = {
+    hardware.opengl = {
       enable = true;
-      wantedBy = [ "multi-user.target" ];
-      path = [ pkgs.qemu ];
-      serviceConfig = {
-        KillMode = lib.mkForce "process";
-        Restart = lib.mkForce "on-failure";
-        RestartSec = lib.mkForce "1s";
-      };
+      driSupport32Bit = true;
     };
 
-    # Set permissions for libvirt socket directory
-    systemd.tmpfiles.rules = [
-      "d /var/run/libvirt 0755 root root -"
-      "d /var/run/libvirt/qemu 0755 root root -"
-    ];
+    # Add user to necessary groups
+    users.users.${cfg.username}.extraGroups = [ "libvirtd" "kvm" ];
+
+    # Polkit rule to allow users in libvirt group to manage VMs
+    security.polkit.extraConfig = ''
+      polkit.addRule(function(action, subject) {
+        if (action.id == "org.libvirt.unix.manage" &&
+            subject.isInGroup("libvirtd")) {
+          return polkit.Result.YES;
+        }
+      });
+    '';
+
+    # Network configuration for libvirt (optional, adjust as needed)
+    networking.firewall.trustedInterfaces = [ "virbr0" ];
+    networking.firewall.allowedUDPPorts = [ 5353 ];
+    networking.firewall.allowedTCPPorts = [ 5900 5901 ];
+
+    # Systemd service to ensure the default network is started
+    systemd.services.libvirtd-default-network = {
+      description = "libvirt default network autostart";
+      after = [ "libvirtd.service" ];
+      requires = [ "libvirtd.service" ];
+      wantedBy = [ "multi-user.target" ];
+      serviceConfig = {
+        Type = "oneshot";
+        RemainAfterExit = true;
+        ExecStart = ''
+          ${pkgs.libvirt}/bin/virsh net-autostart default
+          ${pkgs.libvirt}/bin/virsh net-start default
+        '';
+      };
+    };
   };
 }
