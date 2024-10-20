ddfa91703313dd5e8d196d951aa5b4c918d8f579
Pedro Balbino <phsb5321@gmail.com>
2024-10-20 16:34:36 -0300

    440 current  2024-10-20 16:34:31  24.11.20241018.4c2fcb0  6.11.4                          *

--- hosts/modules/desktop/hyprland/default.nix
+++ hosts/modules/desktop/hyprland/default.nix
@@ -153,17 +153,21 @@ in
             "$mod, down, movefocus, d"
           ] ++ (
             # Workspaces
-            builtins.concatLists (builtins.genList (
-              x: let
-                ws = let
-                  c = (x + 1) / 10;
+            builtins.concatLists (builtins.genList
+              (
+                x:
+                let
+                  ws =
+                    let
+                      c = (x + 1) / 10;
+                    in
+                    builtins.toString (x + 1 - (c * 10));
                 in
-                  builtins.toString (x + 1 - (c * 10));
-              in [
-                "$mod, ${ws}, workspace, ${toString (x + 1)}"
-                "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
-              ]
-            ) 10)
+                [
+                  "$mod, ${ws}, workspace, ${toString (x + 1)}"
+                  "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
+                ]
+              ) 10)
           );
 
           bindm = [
