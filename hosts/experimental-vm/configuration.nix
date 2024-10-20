51ffe08073c0d3e0dcc09f274c8ce4ceff9b55e3
Pedro Balbino <phsb5321@gmail.com>
2024-10-20 16:21:49 -0300

    WORK IN PROGRESS

--- hosts/experimental-vm/configuration.nix
+++ hosts/experimental-vm/configuration.nix
@@ -5,6 +5,7 @@
     ./hardware-configuration.nix
     inputs.home-manager.nixosModules.home-manager
     ../modules/desktop
+    ../modules/virtualization
   ];
 
   # Bootloader configuration
@@ -50,6 +51,14 @@
     };
   };
 
+  # Virtualization module
+  modules.virtualization = {
+    enable = true;
+    enableLibvirtd = true;
+    enableVirtManager = true;
+    username = "notroot";
+  };
+
   console.keyMap = "br-abnt2";
 
   # Define user account
@@ -92,7 +101,6 @@
   home-manager = {
     extraSpecialArgs = { inherit inputs; };
     users.notroot = import ./home.nix;
-    backupFileExtension = "backup";
   };
 
   # Nix settings
@@ -113,26 +121,6 @@
     EDITOR = "nvim";
     VISUAL = "nvim";
   };
-
-  # Enable CUPS for printing
-  services.printing.enable = true;
-
-  # Enable sound with pipewire
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
-  };
-
-  # Enable Bluetooth
-  hardware.bluetooth.enable = true;
-  services.blueman.enable = true;
-
-  # Enable automatic login for the user
-  services.getty.autologinUser = "notroot";
 
   system.stateVersion = "24.05";
 }
--- hosts/experimental-vm/home.nix
+++ hosts/experimental-vm/home.nix
@@ -16,13 +16,8 @@
       (nerdfonts.override { fonts = [ "JetBrainsMono" ]; })
       noto-fonts-emoji
       noto-fonts
-      fish
       eza
       htop
-      neofetch
-      git
-      gh
-      wget
       zoxide
     ];
 
@@ -47,43 +42,6 @@
     ];
   };
 
-  programs.kitty = {
-    enable = true;
-    font = {
-      name = "JetBrainsMono Nerd Font";
-      size = 14;
-    };
-    settings = {
-      copy_on_select = true;
-      enable_ligatures = true;
-      # Tokyo Night theme colors
-      background = "#1a1b26";
-      foreground = "#c0caf5";
-      selection_background = "#33467C";
-      selection_foreground = "#c0caf5";
-      url_color = "#73daca";
-      cursor = "#c0caf5";
-      # normal
-      color0 = "#15161E";
-      color1 = "#f7768e";
-      color2 = "#9ece6a";
-      color3 = "#e0af68";
-      color4 = "#7aa2f7";
-      color5 = "#bb9af7";
-      color6 = "#7dcfff";
-      color7 = "#a9b1d6";
-      # bright
-      color8 = "#414868";
-      color9 = "#f7768e";
-      color10 = "#9ece6a";
-      color11 = "#e0af68";
-      color12 = "#7aa2f7";
-      color13 = "#bb9af7";
-      color14 = "#7dcfff";
-      color15 = "#c0caf5";
-    };
-  };
-
   programs.git = {
     enable = true;
     userName = "Pedro Balbino";
@@ -104,92 +62,20 @@
 
     # General options
     opts = {
-      number = true; # Show line numbers
-      shiftwidth = 2; # Tab width should be 2
-      # Use xclip to copy to clipboard
+      number = true;
+      shiftwidth = 2;
       clipboard = "unnamedplus";
     };
 
     plugins = {
       lualine.enable = true;
-      obsidian = {
-        enable = true;
-        settings = {
-          workspaces = [
-            {
-              name = "Notes";
-              path = "~/Documents/Obsidian/Notes";
-            }
-          ];
-          conceallevel = 2; # Set conceal level (0-2)
-          daily_notes = {
-            folder = "3. Resources/Daily";
-            date_format = "%Y/%B/%d (%A)";
-          };
-        };
-      };
-      treesitter = {
-        enable = true;
-        settings.ensure_installed = "all";
-      };
-      lsp = {
-        enable = true;
-        servers = {
-          rust_analyzer = {
-            enable = true;
-            installCargo = true;
-            installRustc = true;
-          };
-          pyright.enable = true;
-          ts_ls.enable = true;
-          nil_ls.enable = true;
-        };
-      };
-      telescope = {
-        enable = true;
-        keymaps = {
-          "<leader>ff" = "find_files";
-          "<leader>fg" = "live_grep";
-          "<leader>fb" = "buffers";
-          "<leader>fh" = "help_tags";
-        };
-      };
-      gitsigns.enable = true;
-      neo-tree = {
-        enable = true;
-        closeIfLastWindow = true;
-        window.width = 30;
-      };
+      treesitter.enable = true;
+      telescope.enable = true;
       which-key.enable = true;
       comment.enable = true;
       nvim-autopairs.enable = true;
-      neoscroll.enable = true;
-      indent-blankline.enable = true;
-      markdown-preview.enable = true;
-      dashboard.enable = true;
-      web-devicons.enable = true;
-      copilot-vim = {
-        enable = true;
-        settings = { };
-      };
+      gitsigns.enable = true;
     };
-    # Additional settings for Obsidian markdown files
-    extraConfigLua = ''
-      vim.api.nvim_create_autocmd("FileType", {
-        pattern = "markdown",
-        callback = function()
-          vim.opt_local.conceallevel = 2
-          vim.opt_local.concealcursor = 'n'
-        end
-      })
-    '';
-  };
-
-  # Wayland and Hyprland configuration
-  wayland.windowManager.hyprland = {
-    enable = true;
-    systemd.enable = true; # Changed from systemdIntegration
-    xwayland.enable = true;
   };
 
   # Home Manager Self-Management
