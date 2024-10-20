ae17c5fbd55e24a3b12391fb83ee19af2d08d4bd
Pedro Balbino <phsb5321@gmail.com>
2024-10-20 13:46:34 -0300

    WORK IN PROGRESS

--- hosts/modules/desktop/hyprland/default.nix
+++ hosts/modules/desktop/hyprland/default.nix
@@ -108,7 +108,7 @@ in
 
           general {
               gaps_in = 5
-              gaps_out = 20
+              gaps_out = 5
               border_size = 2
               col.active_border = rgba(33ccffee) rgba(00ff99ee) 45deg
               col.inactive_border = rgba(595959aa)
