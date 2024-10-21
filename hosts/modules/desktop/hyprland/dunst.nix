72a43008b1ef38056bfe1f9a4ebb06f1a5d451c4
Pedro Balbino <phsb5321@gmail.com>
2024-10-20 21:36:45 -0300

    WORK IN PROGRESS

--- hosts/modules/desktop/hyprland/dunst.nix
+++ hosts/modules/desktop/hyprland/dunst.nix
@@ -9,7 +9,7 @@ in {
     enable = mkEnableOption "Dunst configuration";
   };
 
-  config = mkIf cfg.enable {
+  config = mkIf (cfg.enable && config.modules.desktop.enable) {
     home-manager.users.${config.modules.desktop.autoLogin.user} = { ... }: {
       services.dunst = {
         enable = true;
