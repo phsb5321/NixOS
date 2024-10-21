d2779eb11edb07743c553460253c6a7f778838c6
Pedro Balbino <phsb5321@gmail.com>
2024-10-20 21:34:30 -0300

    WORK IN PROGRESS

--- hosts/modules/desktop/hyprland/dunst.nix
+++ hosts/modules/desktop/hyprland/dunst.nix
@@ -10,34 +10,36 @@ in {
   };
 
   config = mkIf cfg.enable {
-    services.dunst = {
-      enable = true;
-      settings = {
-        global = {
-          font = "JetBrainsMono Nerd Font 11";
-          frame_width = 2;
-          frame_color = "#89b4fa";
-          separator_color = "frame";
-          corner_radius = 10;
-        };
+    home-manager.users.${config.modules.desktop.autoLogin.user} = { ... }: {
+      services.dunst = {
+        enable = true;
+        settings = {
+          global = {
+            font = "JetBrainsMono Nerd Font 11";
+            frame_width = 2;
+            frame_color = "#89b4fa";
+            separator_color = "frame";
+            corner_radius = 10;
+          };
 
-        urgency_low = {
-          background = "#1e1e2e";
-          foreground = "#cdd6f4";
-          timeout = 5;
-        };
+          urgency_low = {
+            background = "#1e1e2e";
+            foreground = "#cdd6f4";
+            timeout = 5;
+          };
 
-        urgency_normal = {
-          background = "#1e1e2e";
-          foreground = "#cdd6f4";
-          timeout = 10;
-        };
+          urgency_normal = {
+            background = "#1e1e2e";
+            foreground = "#cdd6f4";
+            timeout = 10;
+          };
 
-        urgency_critical = {
-          background = "#1e1e2e";
-          foreground = "#cdd6f4";
-          frame_color = "#f38ba8";
-          timeout = 0;
+          urgency_critical = {
+            background = "#1e1e2e";
+            foreground = "#cdd6f4";
+            frame_color = "#f38ba8";
+            timeout = 0;
+          };
         };
       };
     };
