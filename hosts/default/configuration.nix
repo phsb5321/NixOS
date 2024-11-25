91332b7ed6ad7b0881311063db7c15006ce09471
Pedro Balbino <phsb5321@gmail.com>
2024-11-25 15:00:20 -0300

    527 current  2024-11-25 14:54:05  25.05.20241119.23e89b7  6.12.0                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -203,17 +203,6 @@
       enable = true;
       acceleration = "rocm";
     };
-
-    # Flatpack service configuration
-    flatpak.enable = true;
-  };
-
-  systemd.services.flatpak-repo = {
-    wantedBy = ["multi-user.target"];
-    path = [pkgs.flatpak];
-    script = ''
-      flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-    '';
   };
 
   security.rtkit.enable = true;
