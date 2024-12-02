e51d91e57653480acff7e3506ab138aef3f4744f
Pedro Balbino <phsb5321@gmail.com>
2024-12-02 15:47:37 -0300

    02_Dez_2024

---
+++ hosts/modules/home/default.nix
@@ -0,0 +1,92 @@
+# ~/NixOS/hosts/modules/home/default.nix
+{
+  config,
+  lib,
+  pkgs,
+  inputs,
+  ...
+}:
+with lib; let
+  cfg = config.modules.home;
+in {
+  imports = [
+    ./programs # We'll organize program-specific configurations in separate files
+  ];
+
+  options.modules.home = {
+    enable = mkEnableOption "Home configuration module";
+
+    username = mkOption {
+      type = types.str;
+      default = "notroot";
+      description = "Username for home configuration";
+    };
+
+    hostName = mkOption {
+      type = types.str;
+      description = "Host name for system-specific configurations";
+    };
+
+    extraPackages = mkOption {
+      type = with types; listOf package;
+      default = [];
+      description = "Additional packages to install for the user";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    home-manager = {
+      extraSpecialArgs = {inherit inputs;};
+      backupFileExtension = "bkp";
+
+      users.${cfg.username} = { pkgs, ... }: {
+        imports = [
+          inputs.nixvim.homeManagerModules.nixvim
+        ];
+
+        nixpkgs.config = {
+          allowUnfree = true;
+        };
+
+        # User Configuration
+        home = {
+          username = cfg.username;
+          homeDirectory = "/home/${cfg.username}";
+          stateVersion = "24.05";
+
+          packages = with pkgs; [
+            (nerdfonts.override {fonts = ["JetBrainsMono"];})
+            noto-fonts-emoji
+            noto-fonts
+            noto-fonts-cjk-sans
+            fish
+            kitty
+            grc
+            eza
+            ffmpeg
+            gh
+            brave
+            yazi-unwrapped
+            texlive.combined.scheme-full
+            dbeaver-bin
+            amberol
+            awscli2
+            remmina
+            obsidian
+            d2
+            inputs.nixvim
+            ngrok
+          ] ++ cfg.extraPackages;
+
+          sessionVariables = {
+            EDITOR = "nvim";
+            SHELL = "${pkgs.fish}/bin/fish";
+          };
+        };
+
+        # Enable Home Manager itself
+        programs.home-manager.enable = true;
+      };
+    };
+  };
+}
---
+++ hosts/modules/home/programs/fish.nix
@@ -0,0 +1,50 @@
+# ~/NixOS/hosts/modules/home/programs/fish.nix
+{ config, lib, pkgs, ... }:
+with lib; let
+  cfg = config.modules.home;
+in {
+  config = mkIf cfg.enable {
+    home-manager.users.${cfg.username} = {
+      programs.fish = {
+        enable = true;
+        interactiveShellInit = ''
+          # Initialize zoxide for fish
+          ${pkgs.zoxide}/bin/zoxide init fish | source
+
+          # Zellij Settings
+          set -gx ZELLIJ_AUTO_ATTACH false
+          set -gx ZELLIJ_AUTO_EXIT false
+
+          # Define custom CLI tool "vscatch" to open matching files in VSCode
+          function vscatch
+            for f in $argv; code $f; end
+          end
+
+          # Define custom CLI tool "zedcatch" to open matching files in Zed
+          function zedcatch
+            for f in $argv; zeditor $f; end
+          end
+        '';
+
+        shellAliases = {
+          fishconfig = "source ~/.config/fish/config.fish";
+          textractor = "~/NixOS/user-scripts/textractor.sh";
+          ls = "eza -l --icons";
+          nixswitch = "~/NixOS/user-scripts/nixos-rebuild.sh ${cfg.hostName}";
+          nix-select-shell = "~/NixOS/user-scripts/nix-shell-selector.sh";
+        };
+
+        plugins = [
+          {
+            name = "tide";
+            src = pkgs.fishPlugins.tide.src;
+          }
+          {
+            name = "grc";
+            src = pkgs.fishPlugins.grc.src;
+          }
+        ];
+      };
+    };
+  };
+}
