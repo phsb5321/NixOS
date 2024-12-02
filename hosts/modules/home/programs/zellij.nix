2c8e2e80bfd329e604c2a942a9c8f6ca04ec1d91
Pedro Balbino <phsb5321@gmail.com>
2024-12-02 15:47:37 -0300

    02_Dez_2024

---
+++ hosts/modules/home/programs/zellij.nix
@@ -0,0 +1,17 @@
+# ~/NixOS/hosts/modules/home/programs/zellij.nix
+{ config, lib, pkgs, ... }:
+with lib; let
+  cfg = config.modules.home;
+in {
+  config = mkIf cfg.enable {
+    home-manager.users.${cfg.username} = {
+      programs.zellij = {
+        enable = true;
+        settings = {
+          theme = "one-half-dark";
+          default_shell = "fish";
+        };
+      };
+    };
+  };
+}
