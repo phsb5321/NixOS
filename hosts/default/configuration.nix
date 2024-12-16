1d31ce572e5dfec43ee8161c5bb6f332aaa0ba30
Pedro Balbino <phsb5321@gmail.com>
2024-12-16 16:56:45 -0300

    594 current  2024-12-16 16:56:39  25.05.20241213.3566ab7  6.12.4                          *

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -333,6 +333,6 @@
 
   # Increase Nix download buffer size
   nix.settings = {
-    "download-buffer-size" = "200MiB"; # Further increase if needed
+    "download-buffer-size" = 209715200; # 200 MiB in bytes
   };
 }
