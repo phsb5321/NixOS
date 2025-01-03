6b3123c8f8f8758862f3d5018d888513a745c74e
Pedro Balbino <phsb5321@gmail.com>
2025-01-03 12:51:35 -0300

    rebuild(experimental-vm): 30 current 2025-01-03 12:51:30 25.05.20241229.88195a9 6.6.68 *

--- hosts/experimental-vm/hardware-configuration.nix
+++ hosts/experimental-vm/hardware-configuration.nix
@@ -17,11 +17,16 @@
   boot.kernelModules = [];
   boot.extraModulePackages = [];
 
-  # Updated boot loader configuration
-  boot.loader.grub = {
-    enable = true;
-    device = "/dev/sda"; # Install GRUB to MBR
-    useOSProber = true;
+  # Updated boot loader configuration for UEFI
+  boot.loader = {
+    systemd-boot = {
+      enable = true;
+      consoleMode = "max";
+    };
+    efi = {
+      canTouchEfiVariables = true;
+      efiSysMountPoint = "/boot";
+    };
   };
 
   fileSystems."/" = {
@@ -32,18 +37,12 @@
   fileSystems."/boot" = {
     device = "/dev/disk/by-uuid/5980-E520";
     fsType = "vfat";
-    options = ["fmask=0077" "dmask=0077"];
+    options = ["defaults" "umask=0077" "dmask=0077" "fmask=0077" "utf8" "flush"];
   };
 
   swapDevices = [];
 
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.ens18.useDHCP = lib.mkDefault true;
-  # networking.interfaces.virbr0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
 }
