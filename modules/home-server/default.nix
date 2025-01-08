3c33abcfef5ef292c2bf396faeb14f8457279837
Pedro Balbino <phsb5321@gmail.com>
2025-01-08 20:32:46 -0300

    rebuild(experimental-vm): 67 current 2025-01-08 20:30:32 25.05.20250104.8f3e1f8 6.6.69 *

--- modules/home-server/default.nix
+++ modules/home-server/default.nix
@@ -4,9 +4,6 @@
   lib,
   ...
 }: {
-  ########################################
-  # 1) Define top-level homeServer.enable
-  ########################################
   options.homeServer.enable = lib.mkOption {
     type = lib.types.bool;
     default = false;
@@ -16,30 +13,75 @@
     '';
   };
 
-  ########################################
-  # 2) Import All Submodules
-  ########################################
   imports = [
     ./languagetool.nix
     ./qbittorrent.nix
     ./plex.nix
-    # ./dashy.nix
+    ./homepage.nix
   ];
 
-  ###################################################
-  # 3) Tie each submodule's .enable to homeServer.enable
-  ###################################################
   config = {
-    # LanguageTool (fixed service name)
     services.languagetool.enable = lib.mkDefault config.homeServer.enable;
-
-    # qBittorrent
     services.qbittorrent.enable = lib.mkDefault config.homeServer.enable;
-
-    # Custom Plex (renamed to avoid conflicts)
     services.customPlex.enable = lib.mkDefault config.homeServer.enable;
 
-    # Dashboard (renamed to avoid conflicts)
-    # services.customDashy.enable = lib.mkDefault config.homeServer.enable;
+    services.home-dashboard = {
+      enable = lib.mkDefault config.homeServer.enable;
+      settings = {
+        title = "Home Server Dashboard";
+        theme = "dark";
+        color = "slate";
+        language = "en";
+        headerStyle = "clean";
+      };
+
+      services = [
+        {
+          Media = [
+            {
+              Plex = {
+                href = "http://localhost:32400/web";
+                icon = "plex.png";
+                description = "Media server";
+              };
+            }
+            {
+              qBittorrent = {
+                href = "http://localhost:8080";
+                icon = "qbittorrent.png";
+                description = "Download manager";
+              };
+            }
+          ];
+        }
+        {
+          Tools = [
+            {
+              LanguageTool = {
+                href = "http://localhost:8081";
+                icon = "language.png";
+                description = "Grammar checker API";
+              };
+            }
+          ];
+        }
+      ];
+
+      widgets = [
+        {
+          resources = {
+            cpu = true;
+            memory = true;
+            disk = "/";
+          };
+        }
+        {
+          datetime = {
+            format = "MMMM Do, h:mm a";
+            background = true;
+          };
+        }
+      ];
+    };
   };
 }
