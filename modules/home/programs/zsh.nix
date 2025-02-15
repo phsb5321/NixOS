97ce32bc133ee30a69094cd0507aff49b8313322
Pedro Balbino <pedro.balbino@globalsys.com.br>
2025-02-15 13:07:59 -0300

    rebuild(default): 706 current 2025-02-15 13:07:56 25.05.20250213.2ff53fe 6.13.2 *

--- modules/home/programs/zsh.nix
+++ modules/home/programs/zsh.nix
@@ -26,6 +26,19 @@ in {
             export ZELLIJ_AUTO_ATTACH=false
             export ZELLIJ_AUTO_EXIT=false
 
+            if [[ -z "$ZELLIJ" ]]; then
+                if [[ "$ZELLIJ_AUTO_ATTACH" == "true" ]]; then
+                    zellij attach -c
+                else
+                    zellij
+                fi
+
+                if [[ "$ZELLIJ_AUTO_EXIT" == "true" ]]; then
+                    exit
+                fi
+            fi
+
+
             # ZSH Options
             setopt EXTENDED_HISTORY
             setopt SHARE_HISTORY
