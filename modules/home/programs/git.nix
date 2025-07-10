865eb3b5e516861449364c4994c9823caac58cd0
Pedro Balbino <phsb5321@gmail.com>
2025-07-10 19:56:20 -0300

    [1;38;5;214m📝 Git Commit[0m
    nixos: update configuration (auto-commit after successful rebuild)

--- modules/home/programs/git.nix
+++ modules/home/programs/git.nix
@@ -14,8 +14,40 @@ in {
         userName = "Pedro Balbino";
         userEmail = "phsb5321@gmail.com";
         extraConfig = {
-          core.editor = "nvim";
+          core = {
+            editor = "nvim";
+            autocrlf = "input";
+            safecrlf = true;
+            pager = "delta";
+          };
           init.defaultBranch = "main";
+          pull.rebase = true;
+          push.default = "current";
+          merge.conflictstyle = "diff3";
+          diff.colorMoved = "default";
+          rerere.enabled = true;
+          branch.autosetupmerge = "always";
+          branch.autosetuprebase = "always";
+          
+          # Delta configuration for better diffs
+          delta = {
+            navigate = true;
+            light = false;
+            line-numbers = true;
+            side-by-side = true;
+          };
+          
+          # Better log formatting
+          alias = {
+            lg = "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
+            st = "status -s";
+            co = "checkout";
+            br = "branch";
+            ci = "commit";
+            unstage = "reset HEAD --";
+            last = "log -1 HEAD";
+            visual = "!gitk";
+          };
         };
       };
     };
