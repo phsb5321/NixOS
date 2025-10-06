a7280aa2d82d7f6cbfad73abc8cdfdc100206aed
phsb5321 <phsb5321@users.noreply.github.com>
2025-10-06 14:25:28 -0300

    feat(services): add modular printing service with CUPS and Avahi

--- modules/services/printing.nix
+++ modules/services/printing.nix
@@ -1,13 +1,57 @@
 # ~/NixOS/modules/services/printing.nix
 # Printing service (CUPS) configuration
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
+with lib;
+
+let
+  cfg = config.modules.services.printing;
+in
 {
   options.modules.services.printing = {
-    enable = lib.mkEnableOption "printing service (CUPS)";
+    enable = mkEnableOption "printing service (CUPS)";
+
+    drivers = mkOption {
+      type = types.listOf types.package;
+      default = with pkgs; [
+        gutenprint
+        hplip
+        epson-escpr
+      ];
+      description = "List of printer drivers to install";
+    };
+
+    avahi = {
+      enable = mkOption {
+        type = types.bool;
+        default = true;
+        description = "Enable Avahi for printer discovery";
+      };
+
+      nssmdns4 = mkOption {
+        type = types.bool;
+        default = true;
+        description = "Enable multicast DNS in NSS";
+      };
+
+      openFirewall = mkOption {
+        type = types.bool;
+        default = true;
+        description = "Open firewall for Avahi";
+      };
+    };
   };
 
-  config = lib.mkIf config.modules.services.printing.enable {
-    # Placeholder - will be populated in Task 2.4
+  config = mkIf cfg.enable {
+    services.printing = {
+      enable = true;
+      drivers = cfg.drivers;
+    };
+
+    services.avahi = mkIf cfg.avahi.enable {
+      enable = true;
+      nssmdns4 = cfg.avahi.nssmdns4;
+      openFirewall = cfg.avahi.openFirewall;
+    };
   };
 }
