f6323c2497b29362a8981822113893fb113658d7
phsb5321 <phsb5321@users.noreply.github.com>
2025-10-06 14:51:28 -0300

    feat(roles): add server and minimal role modules

--- modules/roles/server.nix
+++ modules/roles/server.nix
@@ -8,6 +8,30 @@
   };
 
   config = lib.mkIf config.modules.roles.server.enable {
-    # Placeholder - for future use
+    # Server services
+    modules.services = {
+      ssh.enable = true;
+    };
+
+    # Hardware - minimal
+    hardware = {
+      enableRedistributableFirmware = true;
+      bluetooth.enable = false;  # Not needed on server
+      graphics.enable = false;   # Headless
+    };
+
+    # User configuration
+    users.users.notroot = {
+      isNormalUser = true;
+      description = "Pedro Balbino";
+      extraGroups = [
+        "networkmanager" "wheel"
+      ];
+    };
+
+    # Base programs
+    programs = {
+      zsh.enable = true;
+    };
   };
 }
