8814b1efce7f0295c7a21a7fc9085da38f3d88d5
phsb5321 <phsb5321@users.noreply.github.com>
2025-10-06 14:19:54 -0300

    feat(services): add modular syncthing service

--- modules/services/syncthing.nix
+++ modules/services/syncthing.nix
@@ -2,12 +2,54 @@
 # Syncthing file synchronization service
 { config, lib, ... }:
 
+with lib;
+
+let
+  cfg = config.modules.services.syncthing;
+in
 {
   options.modules.services.syncthing = {
-    enable = lib.mkEnableOption "Syncthing file synchronization";
+    enable = mkEnableOption "Syncthing file synchronization";
+
+    user = mkOption {
+      type = types.str;
+      default = "notroot";
+      description = "User to run Syncthing as";
+    };
+
+    dataDir = mkOption {
+      type = types.str;
+      default = "/home/${cfg.user}/Sync";
+      description = "Directory for synchronized files";
+    };
+
+    configDir = mkOption {
+      type = types.str;
+      default = "/home/${cfg.user}/.config/syncthing";
+      description = "Directory for Syncthing configuration";
+    };
+
+    overrideDevices = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Override device configuration";
+    };
+
+    overrideFolders = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Override folder configuration";
+    };
   };
 
-  config = lib.mkIf config.modules.services.syncthing.enable {
-    # Placeholder - will be populated in Task 2.2
+  config = mkIf cfg.enable {
+    services.syncthing = {
+      enable = true;
+      user = cfg.user;
+      dataDir = cfg.dataDir;
+      configDir = cfg.configDir;
+      overrideDevices = cfg.overrideDevices;
+      overrideFolders = cfg.overrideFolders;
+    };
   };
 }
