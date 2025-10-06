aa9c5ba232b22882baa5975a850f92ab06ef72a0
phsb5321 <phsb5321@users.noreply.github.com>
2025-10-06 14:45:54 -0300

    feat(roles): add desktop role module

--- modules/roles/desktop.nix
+++ modules/roles/desktop.nix
@@ -1,13 +1,49 @@
 # ~/NixOS/modules/roles/desktop.nix
 # Desktop role - full features, gaming, development
-{ config, lib, ... }:
+{ config, lib, pkgs, ... }:
 
 {
   options.modules.roles.desktop = {
-    enable = lib.mkEnableOption "desktop role";
+    enable = lib.mkEnableOption "desktop workstation role";
   };
 
   config = lib.mkIf config.modules.roles.desktop.enable {
-    # Placeholder - will be populated in Task 3.2
+    # Services
+    modules.services = {
+      syncthing.enable = true;
+      printing.enable = true;
+      ssh.enable = true;
+    };
+
+    # Dotfiles
+    modules.dotfiles.enable = true;
+
+    # Hardware
+    hardware = {
+      enableRedistributableFirmware = true;
+      bluetooth.enable = true;
+      graphics = {
+        enable = true;
+        enable32Bit = true;
+      };
+    };
+
+    # User configuration
+    users.users.notroot = {
+      isNormalUser = true;
+      description = "Pedro Balbino";
+      extraGroups = [
+        "networkmanager" "wheel" "audio" "video"
+        "disk" "input" "bluetooth" "docker"
+        "render" "kvm" "pipewire"
+      ];
+    };
+
+    # Base programs
+    programs = {
+      zsh.enable = true;
+      dconf.enable = true;
+      nix-ld.enable = true;
+    };
   };
 }
